9bd4de5120894495bcf63e9dc27b64d402997d78
Shaunak Vairagare <shaunakv1@gmail.com>
2014-02-05 17:35:34 -0500

    started adding basic svn commands needed for the project. Also shrinkwraped the dependencies

--- deployer.js
+++ deployer.js
@@ -1,6 +1,26 @@
+var debug = require('./lib/debug.js');
 var Client = require('svn-spawn');
+
 var client = new Client({
-    cwd: '/path to your svn working directory',
-    username: 'username', // optional if authentication not required or is already saved
-    password: 'password', // optional if authentication not required or is already saved
+    cwd: 'L:/httpd/apps/ccapatlas'
 });
+
+// Check the current head revision number
+client.getInfo("-rHEAD",function(err, data) {
+    console.log("Current SVN Head revision:");
+    debug.log(data.commit.$.revision);
+});
+
+//check the working copy's current revision
+client.getInfo(function(err, data) {
+    console.log("Current SVN working copy revision:");
+    debug.log(data.commit.$.revision);
+});
+
+// if there is a mismatch, then working copy is stale
+
+// coming up soon...
+
+
+
+
