d7840e0019a200c845299acc2a569689229751d4
Shaunak Vairagare <shaunakv1@gmail.com>
2014-02-11 18:52:33 -0500

    added svn class. And working on main script

--- config/repos-config.js
+++ config/repos-config.js
@@ -3,12 +3,12 @@
 exports.REPOSITORIES = [
 	{
 		name: 'Project A',
-		repoUrl: 'https://github.com/shaunakv1/Test-Project-A.git',
+		repoUrl: 'https://github.com/shaunakv1/Test-Project-A.git/',
 		branch: 'trunk/'
 	},
 	{
 		name: 'Project B',
-		repoUrl: 'https://github.com/shaunakv1/Test-Project-B.git',
+		repoUrl: 'https://github.com/shaunakv1/Test-Project-B.git/',
 		branch: 'trunk/'
 	}
 ];
--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 
 var db = require('./db/db.js');
 var debug = require('./lib/debug.js');
-var status = require('./model/status.js');
-var config = require('./config/repos-config.js');
-var projects = require('./model/projects.js');
+var status = require('./lib/status.js');
+var projects = require('./lib/projects.js');
+var SvnServer = require('./lib/svn').SvnServer;
 var und = require('underscore');
 
 //start DB
@@ -15,16 +15,24 @@ db.initializeStatusDB();
 //Do for each project in config (might change to  check local checkout directory of all projects)
 	und.each(projects.all(),function (project) {
 		//Check if a project in config has 'status' in DB, if not create one.
+		status.create(project.name, function (err) {
 
+			if(!!err) console.log('Status for - '+ project+'- exists..' );
+			else console.log('Status for - '+ project+'- created..' );
+
+			//TODO:  Check project's deploy.js that should be checked in with the project in the svn.
+			//TODO:  Get the svn info for this project
+			new SvnServer(project.repoUrl+project.branch,function(err,repo){
+				console.log('Project Head Revision:'+ repo.headRevision());
+			});
+			//TODO:  if Head Revision of this project is ahead of curRev of status, deploy this repo's latest copy
+			//TODO:  Update the status to reflect the current
+		});
 	});
 
 
-	//Check project's deploy.js that should be checked in with the project in the svn.
 
-	//Get the svn info for this project
 
-	//if Head Revision of this project is ahead of curRev of status, deploy this repo's latest copy
 
-	//Update the status to reflect the current
 
 
