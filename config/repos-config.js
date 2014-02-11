50a823b341a456059074ffb50921e33de64a1dbc
Shaunak Vairagare <shaunakv1@gmail.com>
2014-02-11 17:18:30 -0500

    Added the new Git hub trial repos as SVN

--- config/repos-config.js
+++ config/repos-config.js
@@ -2,13 +2,13 @@
 
 exports.REPOSITORIES = [
 	{
-		name: 'svn-demo',
-		repoUrl: 'https://csc-s-internals.nos.noaa/svn/svndemo/',
-		branch: 'trunk/svndemo/'
+		name: 'Project A',
+		repoUrl: 'https://github.com/shaunakv1/Test-Project-A.git',
+		branch: 'trunk/'
 	},
 	{
-		name: 'lca_v3',
-		repoUrl: 'https://csc-s-internals.nos.noaa/svn/lca_v3/',
-		branch: 'trunk/lca_v3/'
+		name: 'Project B',
+		repoUrl: 'https://github.com/shaunakv1/Test-Project-B.git',
+		branch: 'trunk/'
 	}
 ];
