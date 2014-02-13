b48236c559931ef57d673a39f28f93cd3701cf69
shaunakv1 <shaunakv1@gmail.com>
2014-02-13 00:13:26 -0500

    added email to the projects

--- config/repos-config.js
+++ config/repos-config.js
@@ -5,12 +5,12 @@ exports.REPOSITORIES = [
 		name: 'Project A',
 		repoUrl: 'https://github.com/shaunakv1/Test-Project-A.git/',
 		branch: 'trunk/',
-		ownerEmail: 'shaunakv1'
+		ownerEmail: 'shaunakv1@gmail.com'
 	},
 	{
 		name: 'Project B',
 		repoUrl: 'https://github.com/shaunakv1/Test-Project-B.git/',
 		branch: 'trunk/',
-		ownerEmail: 'shaunakv1'
+		ownerEmail: 'other@gmail.com'
 	}
 ];
